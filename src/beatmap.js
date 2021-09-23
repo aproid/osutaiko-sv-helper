20a48a51119757b77d60b4ed2108026bb43124f0
Aproid <you171717@naver.com>
2021-09-23 21:04:08 +0900

    Remove inline logging codes

--- src/beatmap.js
+++ src/beatmap.js
@@ -52,14 +52,9 @@ class Beatmap {
 		let { objects, startIndex, endIndex } = this.parseCommaSeperatedTag(this.constructor.TIMING_POINTS_TAG);
 
 		objects = objects.reduce((accumulator, timingPoint) => {
-			try {
 			if(timingPoint.length >= 8)
 				accumulator.push(TimingPoint.fromArray(timingPoint));
-			} catch(e) {
-				console.log(timingPoint);
-				throw e;
-			}
-
+			
 			return accumulator;
 		}, []);
 
