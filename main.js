0b31e2853bcbede109d101650f51ddcefd7d3080
Aproid <you171717@naver.com>
2021-09-23 21:05:09 +0900

    Remove inline logging codes

--- main.js
+++ main.js
@@ -143,7 +143,6 @@ class Main {
 				isBackup: optionBackup
 			});
 		} catch(err) {
-			console.log(err);
 			return this.showMessageBox('error', 'Failed to write beatmap file', 'Couldn\'t write your beatmap file');
 		}
 
@@ -223,7 +222,6 @@ class Main {
 				isBackup: optionBackup
 			});
 		} catch(err) {
-			console.log(err);
 			return this.showMessageBox('error', 'Failed to write beatmap file', 'Couldn\'t write your beatmap file');
 		}
 
